356c992c7d21d3511a21c66b84318b8c65eaf02f
Nogas <Nogas.Jacob@torontorehab.on.ca>
2018-08-16 11:12:18 -0400

    changed path in clstm_ae test

--- c3d_ae_main_test.py
+++ c3d_ae_main_test.py
@@ -2,23 +2,23 @@ from seq_exp import *
 
 
 if __name__ == "__main__":
-        pre_load = None #Set this to path to saved model
+        pre_load = None #Set this to path to saved c3d_ae model
         
+        pre_load = './Models/Thermal-Dummy/dummy_3d.h5'
         if pre_load == None:
                 print('No model path given, please update pre_load variable in dae_main_test.py')
 
         else:
-                img_width, img_height, win_len = 64,64,8
-                hor_flip = False
-                dset = 'UR-Filled'
-                stride = 1
+                img_width, img_height, win_len = 64,64,2
+                
+                dset = 'Thermal-Dummy'
                 img_width, img_height = 64,64
 
                 clstm_ae_exp = SeqExp(dset = dset, pre_load = pre_load, img_width = img_width, \
-                        img_height = img_height, model = model, model_name = model_name)
+                        img_height = img_height, win_len = win_len)
 
 
-                clstm_ae_exp.test(raw = False, animate = True)
+                clstm_ae_exp.test(animate = True)
 
 
 
--- cae_main_test.py
+++ cae_main_test.py
@@ -7,37 +7,37 @@ if __name__ == "__main__":
 	
 	pre_load = None #Put path to your saved model here!! It will be in Models/{dset}/model_name.h5
 
-	pre_load = 'O:/AIRR/FallDetection-current/ThermalFallDetect2017/JNogasPy/Models/UR/CAE-hor_flip-500ep.h5'
-	pre_load = 'O:/AIRR/FallDetection-current/ThermalFallDetect2017/JNogasPy/Models/UR/CAE_Deconv-500ep-hor_flip.hdf5'
+	pre_load = 'O:/AIRR/FallDetection-current/Jacob/Camera-FD//JNogasPy/Models/UR/CAE-hor_flip-500ep.h5'
+	pre_load = 'O:/AIRR/FallDetection-current/Jacob/Camera-FD//JNogasPy/Models/UR/CAE_Deconv-500ep-hor_flip.hdf5'
 
-	pre_load = 'O:/AIRR/FallDetection-current/ThermalFallDetect2017/JNogasPy/Models/UR-Filled/CAE_Deconv-500ep-hor_flip.hdf5'
 
 	
-	pre_load = 'O:/AIRR/FallDetection-current/ThermalFallDetect2017/JNogasPy/Models/CAE-Thermal-epochs_500.h5'
-	pre_load = 'O:/AIRR/FallDetection-current/ThermalFallDetect2017/JNogasPy/Models/CAE-hor_flip-relu-tanh.h5' #TODO remove
-	pre_load = 'O:/AIRR/FallDetection-current/ThermalFallDetect2017/JNogasPy/Models/SDU-Filled/CAE-hor_flip.h5'
+	pre_load = 'O:/AIRR/FallDetection-current/Jacob/Camera-FD//JNogasPy/Models/CAE-Thermal-epochs_500.h5'
+	pre_load = 'O:/AIRR/FallDetection-current/Jacob/Camera-FD//JNogasPy/Models/CAE-hor_flip-relu-tanh.h5' #TODO remove
+	pre_load = 'O:/AIRR/FallDetection-current/Jacob/Camera-FD//JNogasPy/Models/SDU-Filled/CAE-hor_flip.h5'
 	
-	pre_load = 'O:/AIRR/FallDetection-current/ThermalFallDetect2017/JNogasPy/Models/SDU/CAE_Deconv-500-hor_flip.hdf5'
-	pre_load = 'O:/AIRR/FallDetection-current/ThermalFallDetect2017/JNogasPy/Models/UR-Filled/CAE-hor_flip-500ep.h5'
-	pre_load = 'O:/AIRR/FallDetection-current/ThermalFallDetect2017/JNogasPy/Models/SDU-Filled/CAE_Deconv-500ep-hor_flip.hdf5'
+	pre_load = 'O:/AIRR/FallDetection-current/Jacob/Camera-FD//JNogasPy/Models/SDU/CAE_Deconv-500-hor_flip.hdf5'
+	pre_load = 'O:/AIRR/FallDetection-current/Jacob/Camera-FD//JNogasPy/Models/UR-Filled/CAE-hor_flip-500ep.h5'
+	pre_load = 'O:/AIRR/FallDetection-current/Jacob/Camera-FD//JNogasPy/Models/SDU-Filled/CAE_Deconv-500ep-hor_flip.hdf5'
 
-	pre_load = 'O:/AIRR/FallDetection-current/ThermalFallDetect2017/JNogasPy/Models/CAE_Deconv-Dropout-Thermal.h5'
-	pre_load = 'O:/AIRR/FallDetection-current/ThermalFallDetect2017/JNogasPy/Models/CAE-Dropout-Thermal.h5'
-	pre_load = 'O:/AIRR/FallDetection-current/ThermalFallDetect2017/JNogasPy/Models/Thermal/CAE_Deconv-hor_flip-500-0.011.hdf5'
+	pre_load = 'O:/AIRR/FallDetection-current/Jacob/Camera-FD//JNogasPy/Models/CAE_Deconv-Dropout-Thermal.h5'
+	pre_load = 'O:/AIRR/FallDetection-current/Jacob/Camera-FD//JNogasPy/Models/CAE-Dropout-Thermal.h5'
+	pre_load = 'O:/AIRR/FallDetection-current/Jacob/Camera-FD//JNogasPy/Models/Thermal/CAE_Deconv-hor_flip-500-0.011.hdf5'
 
-	#pre_load = 'O:/AIRR/FallDetection-current/ThermalFallDetect2017/JNogasPy/Models/CAE-Dropout_use_gen-True_epochs-200_img_dim-64x64_batch_size-32_horflip-True_Dropout.h5
+	pre_load = 'O:/AIRR/FallDetection-current/Jacob/Camera-FD/JNogasPy/Models/UR-Filled/CAE_Deconv-500ep-hor_flip.hdf5'
+	#pre_load = 'O:/AIRR/FallDetection-current/Jacob/Camera-FD//JNogasPy/Models/CAE-Dropout_use_gen-True_epochs-200_img_dim-64x64_batch_size-32_horflip-True_Dropout.h5
 
 	if pre_load == None:
 		print('No model path given, please update pre_load variable in dae_main_test.py')
 
 	else:
 		hor_flip = True
-		dset = 'Thermal'
+		dset = 'UR-Filled'
 		img_width, img_height = 64,64
 
 		cae_exp = AEExp(pre_load = pre_load, hor_flip = hor_flip, dset = dset,\
 			img_width = img_width, img_height = img_height)
 
-		cae_exp.test(raw = False, animate = False)
+		cae_exp.test(raw = False, animate = True)
 
 
