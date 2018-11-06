62ed24a035b3708a86e79a0e0646bb2fcd214240
Nogas <Nogas.Jacob@torontorehab.on.ca>
2018-11-06 17:53:54 -0500

    added thermal model

--- dae_main_test.py
+++ dae_main_test.py
@@ -4,19 +4,18 @@ from ae_exp import AEExp
 
 if __name__ == "__main__":
 
-	dset = 'UR-Filled'
+	dset = 'Thermal'
 	models_root = './Models/{}'.format(dset)
 	pre_load = None #Put path to your saved model here!! It will be in Models/{dset}/model_name.h5
 
 	pre_load = models_root + '/SDU-Filled/DAE-relu_tanh-Dropout-hor_flip.h5'
 	pre_load = models_root + '/SDU-Filled/DAE-relu_tanh-Dropout-400-hflip.hdf5' #paper? Y
 	pre_load = models_root + '/UR/DAE-relu_tanh-Dropout-hor_flip-500-0.005.hdf5' #paper? Y
-	pre_load = models_root + '/Thermal/DAE-relu_tanh-Dropout-Thermal-epochs_500-hor_flip-PAPER.h5'
 
 	pre_load = models_root + '/DAE-relu_tanh-Dropout-500-hor_flip.hdf5' #paper? Y
+	pre_load = models_root + '/DAE-relu_tanh-Dropout-Thermal-epochs_500.h5'
 	
 
-	pre_load = pre_load
 
 	if pre_load == None:
 		print('No model path given, please update pre_load variable in dae_main_test.py')
